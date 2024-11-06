a8a55f9a1e68b32e93e6aa06fb3658742888e37a
ahmedelkassas <ahmed_elkassas@mivors.com>
2024-11-06 23:25:24 +0200

    chore: Update the 'consulting details' page with more info

--- src/app/[locale]/(general)/consulting/[slug]/page.tsx
+++ src/app/[locale]/(general)/consulting/[slug]/page.tsx
@@ -1,23 +1,9 @@
-"use client";
-
-import PageWrapper from "@/components/PageWrapper";
-import { consultingServices } from "@/lib/data/consultingData";
-import { notFound } from "next/navigation";
+import ConsultingDetails from "@/components/consulting/ConsultingDetails";
 
 export default function ConsultingDetailsPage({
   params: { slug }
 }: {
   params: { slug: string };
 }) {
-  const service = consultingServices?.find((s) => s.slug === slug);
-
-  if (!service) {
-    notFound();
-  }
-
-  return (
-    <PageWrapper sidebarTitle={undefined} sidebarContent={undefined}>
-      <h1 className="text-2xl font-bold mb-4">{service?.title}</h1>
-    </PageWrapper>
-  );
+  return <ConsultingDetails slug={slug} />;
 }
